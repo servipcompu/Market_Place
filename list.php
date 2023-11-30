889eb436c2704e3621fe568d8988e8be2b72a36f
servipcompu <126924544+servipcompu@users.noreply.github.com>
2023-11-30 09:50:45 -0500

    Create list.php

---
+++ list.php
@@ -0,0 +1,195 @@
+kali ini kita kan belajar membangun ecomemr yang terbadi menjadi beebrapa sub folder 
+
+hal-hal penting:
+  ☐ migrasi bs3 ke bs4 karena kita menggunakan bs4 jadi akan banyak sedikti kendala dalam pembuatan tempalate dasar
+
+ yang dikerjakan :
+   ✔ pembuatan navbar berbasis bs4 @done (18-12-12 14:27)
+ 
+rancangan database :
+ ☐ admins
+ ☐ cart
+ ✔ categories @done (18-12-15 20:05)
+ ☐ customers
+ ☐ customer_orders
+ ☐ payment
+ ☐ pending_order
+ ✔ products @done (18-12-15 20:05)
+ ✔ product_categories @done (18-12-15 20:05)
+ ✔ slider @done (18-12-15 20:05)
+
+perbaikan:
+  ✔ header @done (18-12-08 14:30)
+  ✔ navbar @done (18-12-08 14:30)
+  ✔ slider @done (18-12-08 14:30)
+  ✔ box advantage @done (18-12-08 14:30)
+  ✔ tulisan thois week @done (18-12-08 14:30)
+  ✔ product img @done (18-12-08 14:30)
+  ✔ footer @done (18-12-08 14:30)
+  ✔ pada bagian shop.php ada kesalahan pada bagian penbempatan tiap dev product sesuaikan dlu ya @done (18-12-11 16:27)
+  ✔ pada slider details.php image tidak responsive ukuran masih ada yang bersalahan tuh @done (18-12-12 14:27)
+  ☐ clas pada bs4 utnuk menggantikan  form-horizontal
+
+04 Theme structure with html5 & css3 And bs3: @selesai
+  ✔ 049 lecturer 1 @done (18-11-30 23:31)
+  ✔ 50 lecturer 2 @done (18-11-30 23:50)
+  ✔ 51 lecturer 3 @done (18-12-01 00:04)
+  ✔ 52 lecturer 4 mengejakan bagian top navbar @done (18-12-01 09:45)
+  ✔ 53 lecturer 5 @done (18-12-01 10:29)
+  ✔ 54 lecturer 6 @done (18-12-01 11:05)
+  ✔ 55 lecturer 7 @done (18-12-01 11:49)
+  ✔ 56 lecturer 8 navbar html @done (18-12-11 16:27)
+  ✔ 57 lecturer 9 navbar css @done (18-12-11 16:27)
+  ✔ 58 lecturer 10 searchbox on navbar berhenti disini untuk perbaikan beberapa hal @done (18-12-03 10:02)
+  ✔ 59 lecturer 11 slider @done (18-12-11 16:27)
+  ✔ 60 lecturer 12 slider control @done (18-12-11 16:27)
+  ✔ 61 lecturer 13 box dibawah slider @done (18-12-05 16:50)
+  ✔ 62 lecturer 14 @done (18-12-11 16:27)
+  ✔ 63 lecturer 15 pembuatan featured product seprti nya @done (18-12-11 16:27)
+  ✔ 64 lecturer 16 @done (18-12-11 16:27)
+  ✔ 65 lecturer 17 daftar produk yang d jual @done (18-12-11 16:27)
+  ✔ 66 lecturer 18 membuat footer php @done (18-12-11 16:27)
+  ✔ 67 lecturer 19 @done (18-12-11 16:27)
+  ✔ 68 lecturer 20 @done (18-12-11 16:27)
+  ✔ 69 lecturer 21 footer @done (18-12-06 14:37)
+  ✔ 70 lecturer 22 breadcumb @done (18-12-11 16:27)
+  ✔ 71 lecturer 23 bradcumb styles @done (18-12-11 16:27)
+  ✔ 72 lecturer 24 sidebar menu pad menit 08.00 @done (18-12-11 11:22)
+  ✔ 73 lecturer 25 @done (18-12-11 16:27)
+  ✔ 74 lecturer 26 pagination @done (18-12-11 16:27)
+  ✔ 75 lecturer 27 details.php dan slider @done (18-12-11 16:15)
+  ✔ 76 lecturer 28 @done (18-12-11 17:05)
+  ✔ 77 lecturer 29 @done (18-12-11 17:05)
+  ✔ 78 lecturer 30 @done (18-12-11 17:05)
+  ✔ 79 lecturer 31 @done (18-12-11 17:21)
+  ✔ 80 lecturer 32 @done (18-12-11 17:35)
+  ✔ 81 lecturer 33 cart.php @done (18-12-11 23:04)
+  ✔ 82 lecturer 34 @done (18-12-11 23:04)
+  ✔ 83 lecturer 35 @done (18-12-12 09:34)
+  ✔ 84 lecturer 36 @done (18-12-12 09:34)
+  ✔ 85 lecturer 37 @done (18-12-12 09:34)
+  ✔ 86 lecturer 38 contact.php @done (18-12-12 14:55)
+  ✔ 87 lecturer 39 @done (18-12-12 14:55)
+  ✔ 88 lecturer 40 register page @done (18-12-12 15:08)
+  ✔ 89 lecturer 41 my account @done (18-12-12 21:53)
+  ✔ 90 lecturer 42 @done (18-12-13 10:49)
+  ✔ 91 lecturer 43 @done (18-12-13 10:49)
+  ✔ 92 lecturer 44 @done (18-12-13 10:49)
+  ✔ 93 lecturer 45 @done (18-12-13 11:01)
+  ✔ 94 lecturer 46 @done (18-12-13 17:52)
+  ✔ 95 lecturer 47 @done (18-12-13 17:52)
+  ✔ 96 lecturer 48 confirm page @done (18-12-13 17:52)
+  ✔ 97 lecturer 49 @done (18-12-13 17:57)
+  ✔ 98 lecturer 50 @done (18-12-13 18:08)
+  ✔ 99 lecturer 51 @done (18-12-13 18:16)
+
+05 Making E-Commerce Store Dynamic Through PHPMYSQLI :
+ ✔ 100 Lectures 52 @done (18-12-15 11:56) slider dynamic
+ ✔ 101 Lectures 53 @done (18-12-15 19:00)
+ ✔ 102 Lectures 54 @done (18-12-15 19:30)
+ ✔ 103 Lectures 55 @done (18-12-15 19:51)
+ ✔ 104 Lectures 56 form isert data @done (18-12-15 20:11)
+ ✔ 105 Lectures 57 form row @done (18-12-17 10:13)
+ ✔ 106 Lectures 58 @done (18-12-17 10:35)
+ ✔ 107 Lectures 59 insert product @done (18-12-17 10:51)
+ ✔ 108 Lectures 60 @done (18-12-17 11:13)
+ ☐ 109 Lectures 61
+ ☐ 110 Lectures 62
+ ☐ 111 Lectures 63
+ ☐ 112 Lectures 64
+ ☐ 113 Lectures 65
+ ☐ 113 Lectures 66
+ ☐ 113 Lectures 67
+ ☐ 113 Lectures 68
+ ☐ 113 Lectures 69
+ ☐ 113 Lectures 70
+ ☐ 113 Lectures 71
+ ☐ 113 Lectures 72
+ ☐ 113 Lectures 73
+ ☐ 113 Lectures 74
+ ☐ 113 Lectures 75
+ ☐ 113 Lectures 76
+ ☐ 113 Lectures 77
+ ☐ 113 Lectures 78
+ ☐ 113 Lectures 79
+ ☐ 113 Lectures 80
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 104 Lectures 
+ ☐ 105 Lectures 
+ ☐ 106 Lectures 
+ ☐ 107 Lectures 
+ ☐ 108 Lectures 
+ ☐ 109 Lectures 
+ ☐ 110 Lectures 
+ ☐ 111 Lectures 
+ ☐ 112 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 104 Lectures 
+ ☐ 105 Lectures 
+ ☐ 106 Lectures 
+ ☐ 107 Lectures 
+ ☐ 108 Lectures 
+ ☐ 109 Lectures 
+ ☐ 110 Lectures 
+ ☐ 111 Lectures 
+ ☐ 112 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
+ ☐ 113 Lectures 
